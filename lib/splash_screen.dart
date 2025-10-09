dc0e526c0f798e9a15af85e2672dbd2993854b28
HP <ziaullah@5737@gmail.com>
2025-10-10 01:31:27 +0500

    add splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -6,6 +6,8 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    return  Scaffold(
+      body: Column(children: [],),
+    );
   }
 }
